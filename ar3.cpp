9a5716dcc7485f89771c5680ae32fc305f34154e
rohitkumardas9434-dot <rohitkumardas9434@gmail.com>
2026-09-05 17:32:44 +0530

    remove duplicates

--- ar3.cpp
+++ ar3.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 int main()
 {
-    cout<<"arun";
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int a[n];
+    cout << "Enter " << n << " elements: ";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int j=0;
+    for(int i=1;i<n;i++){
+
+        if(a[i]!=a[j]){
+            j++;
+            a[j]=a[i];
+        }
+    }
+    cout << "Array after removing duplicates: ";
+    for(int i = 0; i <= j; i++)
+    {
+        cout << a[i] << " ";
+    }
     return 0;
 }
