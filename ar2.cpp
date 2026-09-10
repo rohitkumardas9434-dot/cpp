944d5fa05ef43152c951c0194cc7860606696313
rohitkumardas9434-dot <rohitkumardas9434@gmail.com>
2026-09-10 21:06:41 +0530

    Array is sorted or not

--- ar2.cpp
+++ ar2.cpp
@@ -25,7 +25,6 @@ int main()
         if(sorted==false)
         {
             cout<<"The array is not sorted in non-increasing order."<<endl;
-            //ahsifhie
         }
         else
         {
